cb9bb1c4a5254bbe8da1a7cb3d8c591effa94559
Christian Pérez <christiandp@live.com>
2022-09-25 23:13:20 -0600

    Players info during game implemented

--- src/components/Memory/memory.js
+++ src/components/Memory/memory.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './memory.scss';
 import Card from './card';
+// import ChatPopUp from '../Chat/ChatPopUp';
 
 const cardImages = [
     {"src": "/img/burro.svg", matched: false },
@@ -12,9 +13,20 @@ const cardImages = [
     {"src": "/img/penguin.svg", matched: false },
     {"src": "/img/serpiente.svg", matched: false },
     {"src": "/img/tortuga.svg", matched: false },
-    {"src": "/img/ardilla.svg", matched: false }
+    {"src": "/img/ardilla.svg", matched: false },
+    {"src": "/img/huella.svg", matched: false },
+    {"src": "/img/aguila.svg", matched: false },
+    {"src": "/img/dog.svg", matched: false },
+    {"src": "/img/rana.svg", matched: false },
+    {"src": "/img/vaca.svg", matched: false },
+    {"src": "/img/gallina.svg", matched: false },
+    {"src": "/img/obeja.svg", matched: false }
 ];
 
+const ply = [
+    {'name': 'Player 1', 'score': 0},
+    {'name': 'Player 2', 'score': 0},
+];
 
 function Memory() {
   const [cards, setCards] = useState([]);
@@ -22,6 +34,8 @@ function Memory() {
   const [choice1, setChoice1] = useState(null);
   const [choice2, setChoice2] = useState(null);
   const [disabled, setDisabled] = useState(false);
+  const [turn, setTurn] = useState(0);
+  const [playerturn, setPlayerTurn] = useState(true);
 
 
   const shuffleCards = () => {
@@ -32,6 +46,7 @@ function Memory() {
     setChoice1(null);
     setChoice2(null);
     setCards(shuffleCards);
+    setPlayers(ply);
     };
 
     const handleChoice = (card) => {
@@ -51,6 +66,15 @@ function Memory() {
                         }
                     });
                 });
+                setPlayers(prevPlayers => {
+                    return prevPlayers.map(player => {
+                        if(player.name === 'Player 1') {
+                            return {...player, score: player.score + 1};
+                        } else {
+                            return player;
+                        }
+                    });
+                });
             } else {
                 console.log('no match');
             }
@@ -58,10 +82,13 @@ function Memory() {
         }
     }, [choice1, choice2]);
 
+    console.log(players);
+
     const resetTurn = () => {
         setChoice1(null);
         setChoice2(null);
         setDisabled(false);
+        setTurn(turn + 1);
     };
 
     useEffect(() => {
@@ -70,20 +97,51 @@ function Memory() {
 
   return (
     <div>
-        <div className='memory'>
-            <h1>Memory Game</h1>
-            <div className='card-grid'>
-                {cards.map(card => (
-                    <Card 
-                        key={card.id} 
-                        card={card} 
-                        handleChoice={handleChoice} 
-                        flipped={card === choice1 || card === choice2 || card.matched} 
-                        disabled={disabled}
-                    />
-                ))}
+        <div className='container-xxl'>
+            <div className='row gx-5'>
+                <div className='col-2 info-player'>
+                    <h4>Info Jugadores</h4>
+                    <div>
+                    <table>
+                        <tr>
+                            <th>Name</th>
+                            <th>Score</th>
+                        </tr>
+                        {players.map((player, key) => {
+                        return (
+                            <tr key={key}>
+                            <td>{player.name}</td>
+                            <td>{player.score}</td>
+                            </tr>
+                        )
+                        })}
+                    </table>
+                    </div>
+                    <div className='esp'>
+                        <h5>Turns: {turn}</h5>
+                    </div>                    
+                </div>
+                <div className='col-8'>           
+                    <div className='memory'>
+                        <h1>Memory Game</h1>
+                        <div className='card-grid'>
+                            {cards.map(card => (
+                                <Card 
+                                    key={card.id} 
+                                    card={card} 
+                                    handleChoice={handleChoice} 
+                                    flipped={card === choice1 || card === choice2 || card.matched} 
+                                    disabled={disabled}
+                                />
+                            ))}
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className='col-2 info-chat'>
+                    <h4>Chat</h4>
+                </div>
             </div>
-            
         </div>
     </div>
   );
